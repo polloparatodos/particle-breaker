252a6b6f10a938103c6ae75e80a4e2da8c25eba0
Ryan <kg5zoq+github@protonmail.ch>
2025-04-27 03:05:36 -0500

    Clean up code consistency and bump minor version from 3.0 to 3.1

--- Bryter.cpp
+++ Bryter.cpp
@@ -34,6 +34,7 @@ const float BOUNDARY_BOTTOM = 1.0f;
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
+// Brick Types and States
 enum BRICKTYPE { REFLECTIVE, DESTRUCTABLE };
 enum ONOFF { ON, OFF };
 
@@ -41,14 +42,14 @@ void processInput(GLFWwindow* window);
 
 template <typename T>
 T GetRandomValue() {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_real_distribution<T> dis(0.0f, 1.0f);
-	return dis(gen);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<T> dis(0.0f, 1.0f);
+    return dis(gen);
 }
 
-float GetRandomValue(){
-	return GetRandomValue<float>();
+float GetRandomValue() {
+    return GetRandomValue<float>();
 }
 
 int SetMovementDirection(int direction) {
@@ -67,162 +68,162 @@ int SetMovementDirection(int direction) {
 }
 
 class Brick {
-	private:
-		float red, green, blue;
-		float x, y, width;
-		BRICKTYPE brick_type;
-		ONOFF onoff;
-	
-	public:
-		Brick(BRICKTYPE bt, float xx, float yy, float ww, float rr, float gg, float bb)
-			: brick_type(bt), x(xx), y(yy), width(ww), red(rr), green(gg), blue(bb), onoff(ON) {}
-	
-		void drawBrick() const {
-			if (onoff == ON) {
-				float halfside = width / 2;
-	
-				glColor3d(red, green, blue);
-				glBegin(GL_POLYGON);
-				glVertex2d(x + halfside, y + halfside);
-				glVertex2d(x + halfside, y - halfside);
-				glVertex2d(x - halfside, y - halfside);
-				glVertex2d(x - halfside, y + halfside);
-				glEnd();
-			}
-		}
-	
-		void hit() {
-			if (brick_type == DESTRUCTABLE) {
-				width /= 2;
-				if (width < MIN_BRICK_WIDTH) {
-					onoff = OFF;
-				}
-				setColor(GetRandomValue(), GetRandomValue(), GetRandomValue());
-			}
-		}
-	
-		bool isColliding(float ballX, float ballY, float ballRadius) const {
-			float halfside = width / 2;
-			return (ballX + ballRadius > x - halfside && ballX - ballRadius < x + halfside &&
-					ballY + ballRadius > y - halfside && ballY - ballRadius < y + halfside);
-		}
-	
-		void setColor(float r, float g, float b) {
-			red = r;
-			green = g;
-			blue = b;
-		}
-	
-		BRICKTYPE getBrickType() const { return brick_type; }
-		ONOFF setOnOff() const { return onoff; }
-		void setBrickType(BRICKTYPE type) { brick_type = type; }
+private:
+    float red, green, blue;
+    float x, y, width;
+    BRICKTYPE brick_type;
+    ONOFF onoff;
 
-		float getWidth() const { return width; }
-		float getX() const { return x; }
-		float getY() const { return y; }
-	};
-	
-	class Circle {
-	private:
-		float red, green, blue;
-		float radius;
-		float x, y;
-		float speed = MIN_SPEED;
-		int direction;
+public:
+    Brick(BRICKTYPE bt, float xx, float yy, float ww, float rr, float gg, float bb)
+        : brick_type(bt), x(xx), y(yy), width(ww), red(rr), green(gg), blue(bb), onoff(ON) {}
 
-		// Move logic for a specific direction
-		void handleDirection(int direction, float& x, float& y, float radius, float speed) {
-			auto move = [&](float& coord, float limit, float delta) {
-				if ((delta > 0 && coord < limit) || (delta < 0 && coord > limit)) {
-					coord += delta;
-				} else {
-					this->direction = SetMovementDirection(-1);
-				}
-			};
-	
-			if (direction == 1 || direction == 5 || direction == 6) {
-				move(y, BOUNDARY_TOP + radius, -speed); // Up
-			}
-			if (direction == 2 || direction == 5 || direction == 7) {
-				move(x, BOUNDARY_RIGHT - radius, speed); // Right
-			}
-			if (direction == 3 || direction == 7 || direction == 8) {
-				move(y, BOUNDARY_BOTTOM - radius, speed); // Down
-			}
-			if (direction == 4 || direction == 6 || direction == 8) {
-				move(x, BOUNDARY_LEFT + radius, -speed); // Left
-			}
-		}
-	
-	public:
-		Circle(double xx, float yy, double rr, int dir, float rad, float r, float g, float b)
-			: x(xx), y(yy), radius(rad), direction(dir), red(r), green(g), blue(b) {}
-	
-		void changeColor() {
-			red = GetRandomValue();
-			green = GetRandomValue();
-			blue = GetRandomValue();
-		}
-	
-		void CheckCollision(Brick* brick) {
-			if (!brick || brick->getWidth() <= 0) {
-				return; // Invalid brick, skip collision
-			}
-	
-			if (brick->isColliding(x, y, radius)) {
-				if (brick->getBrickType() == REFLECTIVE) {
-					speed -= SPEED_DELTA;
-					if (speed < MIN_SPEED) {
-						speed = MIN_SPEED;
-					}
-					brick->setBrickType(DESTRUCTABLE);
-					direction = SetMovementDirection(direction);
-				} else if (brick->getBrickType() == DESTRUCTABLE) {
-					speed += SPEED_DELTA;
-					if (speed > MAX_SPEED) {
-						speed = MAX_SPEED;
-					}
-					brick->hit();
-					changeColor();
-				}
-			}
-		}
-	
-		void MoveOneStep() {
-			// Introduce a "buzzing" effect by randomly changing the direction slightly
-			if (rand() % RANDOM_DIRECTION_PROBABILITY == 0) { // 10% chance to change direction on each step
-				direction = SetMovementDirection(-1);
-			}
-		
-			auto move = [&](float& coord, float limit, float delta) {
-				if ((delta > 0 && coord < limit) || (delta < 0 && coord > limit)) {
-					coord += delta;
-				} else {
-					direction = SetMovementDirection(-1);
-				}
-			};
+    void drawBrick() const {
+        if (onoff == ON) {
+            float halfside = width / 2;
 
-			handleDirection(direction, x, y, radius, speed);
-		}
-	
-		void DrawCircle() const {
-			glColor3f(red, green, blue);
-			glBegin(GL_POLYGON);
-			for (int i = 0; i < CIRCLE_SEGMENTS; i++) {
-				float degInRad = i * DEG2RAD;
-			    // This produces the "buzzing" effect of the ball, simulating the kinetic energy transfer
-			    // to the ball as it bounces off the bricks. The faster it goes, the more it "buzzes"
-			    SetMovementDirection(-1);
-				glVertex2f((cos(degInRad) * radius) + x, (sin(degInRad) * radius) + y);
-			}
-			glEnd();
-		}
-	};
+            glColor3d(red, green, blue);
+            glBegin(GL_POLYGON);
+            glVertex2d(x + halfside, y + halfside);
+            glVertex2d(x + halfside, y - halfside);
+            glVertex2d(x - halfside, y - halfside);
+            glVertex2d(x - halfside, y + halfside);
+            glEnd();
+        }
+    }
+
+    void hit() {
+        if (brick_type == DESTRUCTABLE) {
+            width /= 2;
+            if (width < MIN_BRICK_WIDTH) {
+                onoff = OFF;
+            }
+            setColor(GetRandomValue(), GetRandomValue(), GetRandomValue());
+        }
+    }
+
+    bool isColliding(float ballX, float ballY, float ballRadius) const {
+        float halfside = width / 2;
+        return (ballX + ballRadius > x - halfside && ballX - ballRadius < x + halfside &&
+                ballY + ballRadius > y - halfside && ballY - ballRadius < y + halfside);
+    }
+
+    void setColor(float r, float g, float b) {
+        red = r;
+        green = g;
+        blue = b;
+    }
+
+    BRICKTYPE getBrickType() const { return brick_type; }
+    ONOFF setOnOff() const { return onoff; }
+    void setBrickType(BRICKTYPE type) { brick_type = type; }
+
+    float getWidth() const { return width; }
+    float getX() const { return x; }
+    float getY() const { return y; }
+};
+
+class Circle {
+private:
+    float red, green, blue;
+    float radius;
+    float x, y;
+    float speed = MIN_SPEED;
+    int direction;
+
+    // Move logic for a specific direction
+    void handleDirection(int direction, float& x, float& y, float radius, float speed) {
+        auto move = [&](float& coord, float limit, float delta) {
+            if ((delta > 0 && coord < limit) || (delta < 0 && coord > limit)) {
+                coord += delta;
+            } else {
+                this->direction = SetMovementDirection(-1);
+            }
+        };
+
+        if (direction == 1 || direction == 5 || direction == 6) {
+            move(y, BOUNDARY_TOP + radius, -speed); // Up
+        }
+        if (direction == 2 || direction == 5 || direction == 7) {
+            move(x, BOUNDARY_RIGHT - radius, speed); // Right
+        }
+        if (direction == 3 || direction == 7 || direction == 8) {
+            move(y, BOUNDARY_BOTTOM - radius, speed); // Down
+        }
+        if (direction == 4 || direction == 6 || direction == 8) {
+            move(x, BOUNDARY_LEFT + radius, -speed); // Left
+        }
+    }
+
+public:
+    Circle(double xx, float yy, double rr, int dir, float rad, float r, float g, float b)
+        : x(xx), y(yy), radius(rad), direction(dir), red(r), green(g), blue(b) {}
+
+    void changeColor() {
+        red = GetRandomValue();
+        green = GetRandomValue();
+        blue = GetRandomValue();
+    }
+
+    void CheckCollision(Brick* brick) {
+        if (!brick || brick->getWidth() <= 0) {
+            return; // Invalid brick, skip collision
+        }
+
+        if (brick->isColliding(x, y, radius)) {
+            if (brick->getBrickType() == REFLECTIVE) {
+                speed -= SPEED_DELTA;
+                if (speed < MIN_SPEED) {
+                    speed = MIN_SPEED;
+                }
+                brick->setBrickType(DESTRUCTABLE);
+                direction = SetMovementDirection(direction);
+            } else if (brick->getBrickType() == DESTRUCTABLE) {
+                speed += SPEED_DELTA;
+                if (speed > MAX_SPEED) {
+                    speed = MAX_SPEED;
+                }
+                brick->hit();
+                changeColor();
+            }
+        }
+    }
+
+    void MoveOneStep() {
+        // Introduce a "buzzing" effect by randomly changing the direction slightly
+        if (rand() % RANDOM_DIRECTION_PROBABILITY == 0) { // 10% chance to change direction on each step
+            direction = SetMovementDirection(-1);
+        }
+
+        auto move = [&](float& coord, float limit, float delta) {
+            if ((delta > 0 && coord < limit) || (delta < 0 && coord > limit)) {
+                coord += delta;
+            } else {
+                direction = SetMovementDirection(-1);
+            }
+        };
+
+        handleDirection(direction, x, y, radius, speed);
+    }
+
+    void DrawCircle() const {
+        glColor3f(red, green, blue);
+        glBegin(GL_POLYGON);
+        for (int i = 0; i < CIRCLE_SEGMENTS; i++) {
+            float degInRad = i * DEG2RAD;
+			// This produces the "buzzing" effect of the ball, simulating the kinetic energy transfer
+			// to the ball as it bounces off the bricks. The faster it goes, the more it "buzzes"
+			SetMovementDirection(-1);
+            glVertex2f((cos(degInRad) * radius) + x, (sin(degInRad) * radius) + y);
+        }
+        glEnd();
+    }
+};
 
 std::vector<Circle> world;
 
-Brick CreateBrick(BRICKTYPE type, float x, float y, float width){
-	return Brick(type, x, y, width, GetRandomValue(), GetRandomValue(), GetRandomValue());
+Brick CreateBrick(BRICKTYPE type, float x, float y, float width) {
+    return Brick(type, x, y, width, GetRandomValue(), GetRandomValue(), GetRandomValue());
 }
 
 void InitializeBricks(std::vector<Brick>& bricks) {
@@ -242,73 +243,74 @@ void InitializeBricks(std::vector<Brick>& bricks) {
 }
 
 int main(void) {
-	srand(time(NULL));
+    srand(time(NULL));
 
-	if (!glfwInit()) {
-		std::cerr << "Failed to initialize GLFW" << std::endl;
-		::exit(EXIT_FAILURE);
-	}
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pseudo Kinetic Molecular Theory Simulator", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
-		::exit(EXIT_FAILURE);
-	}
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        ::exit(EXIT_FAILURE);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pseudo Kinetic Molecular Theory Simulator", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        ::exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-	std::vector<Brick> bricks;
-	InitializeBricks(bricks);
-	
-	while (!glfwWindowShouldClose(window)) {
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		glViewport(0, 0, width, height);
-		// Explicitly set the background color to black
+    std::vector<Brick> bricks;
+    InitializeBricks(bricks);
+
+    while (!glfwWindowShouldClose(window)) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        // Explicitly set the background color to black
 		// This is the color of the window, not the background of the scene
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		processInput(window);
+        processInput(window);
 
-		// Movement and collision detection
-		for (int i = 0; i < world.size(); i++) {
-			for (auto& brick : bricks) {
-				world[i].CheckCollision(&brick);
-			}
-			world[i].MoveOneStep();
-			world[i].DrawCircle();
-		}
+        // Movement and collision detection
+        for (int i = 0; i < world.size(); i++) {
+            for (auto& brick : bricks) {
+                world[i].CheckCollision(&brick);
+            }
+            world[i].MoveOneStep();
+            world[i].DrawCircle();
+        }
 
-		// Remove destroyed bricks from the vector
-		bricks.erase(std::remove_if(bricks.begin(), bricks.end(), [](const Brick& brick) {
-			return brick.setOnOff() == OFF;
-		}), bricks.end());
+        // Remove destroyed bricks from the vector
+        bricks.erase(std::remove_if(bricks.begin(), bricks.end(), [](const Brick& brick) {
+            return brick.setOnOff() == OFF;
+        }), bricks.end());
 
-		for (auto& brick : bricks) {
-			brick.drawBrick();
-		}
+        for (auto& brick : bricks) {
+            brick.drawBrick();
+        }
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	::exit(EXIT_SUCCESS);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    ::exit(EXIT_SUCCESS);
 }
 
 void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-	{
-		float r = GetRandomValue(), g = GetRandomValue(), b = GetRandomValue();
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        float r = GetRandomValue(), g = GetRandomValue(), b = GetRandomValue();
 
-		// Since we're simulating Kinetic Molecular Theory, direction must be random
+        // Since we're simulating Kinetic Molecular Theory, direction must be random
 		// I mean, it's not really KMT, but it is a fun way to think about it
-		world.emplace_back(CIRCLE_START_X, CIRCLE_START_Y, INITIAL_RADIUS, SetMovementDirection(-1), 0.05, r, g, b);
-	}
+        world.emplace_back(CIRCLE_START_X, CIRCLE_START_Y, INITIAL_RADIUS, SetMovementDirection(-1), 0.05, r, g, b);
+    }
 }
